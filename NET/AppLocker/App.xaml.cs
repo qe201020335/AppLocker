cef1a8b0db412fb6cd2297aab3e55f82d4d48e0d
LemonU <lassaegsi@gmail.com>
2021-01-24 21:32:17 -0500

    First proof-of-concept prototype

---
+++ NET/AppLocker/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace AppLocker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private const string steamPath = @"C:\Users\Lemon\Desktop\steam.exe",
+                             savePath = @"C:\Users\Lemon\Desktop\DataChunk";
+        public static String GetNewString() { return "YEET"; }
+
+        public static void LockApp()
+        {
+            TransferBinaries(steamPath, savePath);
+        }
+
+        public static void ReleaseApp()
+        {
+            TransferBinaries(savePath, steamPath);
+        }
+
+        public static void TransferBinaries(string src, string des)
+        {
+            var binaries = File.ReadAllBytes(src);
+            File.WriteAllBytes(des, binaries);
+            File.Delete(src);
+        }
+    }
+}
---
+++ NET/AppLocker/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppLocker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //String sDate = myCalendar.SelectedDate.ToString();
+            String sDate = DateTime.Now.ToString();
+            textBlock.Text = sDate;
+            Console.WriteLine(sDate);
+
+            //button.Content = "YEET";
+        }
+
+        private void lockButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.LockApp();
+        }
+
+        private void releaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.ReleaseApp();
+        }
+    }
+}
